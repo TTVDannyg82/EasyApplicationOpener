e0262a38eee4a7ace5d240d52dbf19907498d200
TTVDannyg82 <63794287+TTVDannyg82@users.noreply.github.com>
2022-01-05 00:47:12 +0000

    Add files via upload

---
+++ Form2.Designer.cs
@@ -0,0 +1,251 @@
+﻿
+namespace NBT_Maker
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.siticoneButton1 = new Siticone.UI.WinForms.SiticoneButton();
+            this.siticoneButton2 = new Siticone.UI.WinForms.SiticoneButton();
+            this.siticoneButton3 = new Siticone.UI.WinForms.SiticoneButton();
+            this.siticoneButton4 = new Siticone.UI.WinForms.SiticoneButton();
+            this.siticoneButton5 = new Siticone.UI.WinForms.SiticoneButton();
+            this.siticoneButton6 = new Siticone.UI.WinForms.SiticoneButton();
+            this.siticoneButton7 = new Siticone.UI.WinForms.SiticoneButton();
+            this.siticoneButton8 = new Siticone.UI.WinForms.SiticoneButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.siticoneButton9 = new Siticone.UI.WinForms.SiticoneButton();
+            this.siticoneDragControl1 = new Siticone.UI.WinForms.SiticoneDragControl(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(12, 421);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Dannyg82#6969";
+            // 
+            // siticoneButton1
+            // 
+            this.siticoneButton1.CheckedState.Parent = this.siticoneButton1;
+            this.siticoneButton1.CustomImages.Parent = this.siticoneButton1;
+            this.siticoneButton1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.siticoneButton1.ForeColor = System.Drawing.Color.White;
+            this.siticoneButton1.HoveredState.Parent = this.siticoneButton1;
+            this.siticoneButton1.Location = new System.Drawing.Point(12, 297);
+            this.siticoneButton1.Name = "siticoneButton1";
+            this.siticoneButton1.ShadowDecoration.Parent = this.siticoneButton1;
+            this.siticoneButton1.Size = new System.Drawing.Size(180, 45);
+            this.siticoneButton1.TabIndex = 3;
+            this.siticoneButton1.Text = "Badlion";
+            this.siticoneButton1.Click += new System.EventHandler(this.siticoneButton1_Click);
+            // 
+            // siticoneButton2
+            // 
+            this.siticoneButton2.CheckedState.Parent = this.siticoneButton2;
+            this.siticoneButton2.CustomImages.Parent = this.siticoneButton2;
+            this.siticoneButton2.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.siticoneButton2.ForeColor = System.Drawing.Color.White;
+            this.siticoneButton2.HoveredState.Parent = this.siticoneButton2;
+            this.siticoneButton2.Location = new System.Drawing.Point(12, 246);
+            this.siticoneButton2.Name = "siticoneButton2";
+            this.siticoneButton2.ShadowDecoration.Parent = this.siticoneButton2;
+            this.siticoneButton2.Size = new System.Drawing.Size(180, 45);
+            this.siticoneButton2.TabIndex = 4;
+            this.siticoneButton2.Text = "Discord";
+            this.siticoneButton2.Click += new System.EventHandler(this.siticoneButton2_Click);
+            // 
+            // siticoneButton3
+            // 
+            this.siticoneButton3.CheckedState.Parent = this.siticoneButton3;
+            this.siticoneButton3.CustomImages.Parent = this.siticoneButton3;
+            this.siticoneButton3.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.siticoneButton3.ForeColor = System.Drawing.Color.White;
+            this.siticoneButton3.HoveredState.Parent = this.siticoneButton3;
+            this.siticoneButton3.Location = new System.Drawing.Point(12, 195);
+            this.siticoneButton3.Name = "siticoneButton3";
+            this.siticoneButton3.ShadowDecoration.Parent = this.siticoneButton3;
+            this.siticoneButton3.Size = new System.Drawing.Size(180, 45);
+            this.siticoneButton3.TabIndex = 5;
+            this.siticoneButton3.Text = "IP Research";
+            this.siticoneButton3.Click += new System.EventHandler(this.siticoneButton3_Click);
+            // 
+            // siticoneButton4
+            // 
+            this.siticoneButton4.CheckedState.Parent = this.siticoneButton4;
+            this.siticoneButton4.CustomImages.Parent = this.siticoneButton4;
+            this.siticoneButton4.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.siticoneButton4.ForeColor = System.Drawing.Color.White;
+            this.siticoneButton4.HoveredState.Parent = this.siticoneButton4;
+            this.siticoneButton4.Location = new System.Drawing.Point(12, 144);
+            this.siticoneButton4.Name = "siticoneButton4";
+            this.siticoneButton4.ShadowDecoration.Parent = this.siticoneButton4;
+            this.siticoneButton4.Size = new System.Drawing.Size(180, 45);
+            this.siticoneButton4.TabIndex = 6;
+            this.siticoneButton4.Text = "VSC";
+            this.siticoneButton4.Click += new System.EventHandler(this.siticoneButton4_Click);
+            // 
+            // siticoneButton5
+            // 
+            this.siticoneButton5.CheckedState.Parent = this.siticoneButton5;
+            this.siticoneButton5.CustomImages.Parent = this.siticoneButton5;
+            this.siticoneButton5.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.siticoneButton5.ForeColor = System.Drawing.Color.White;
+            this.siticoneButton5.HoveredState.Parent = this.siticoneButton5;
+            this.siticoneButton5.Location = new System.Drawing.Point(12, 93);
+            this.siticoneButton5.Name = "siticoneButton5";
+            this.siticoneButton5.ShadowDecoration.Parent = this.siticoneButton5;
+            this.siticoneButton5.Size = new System.Drawing.Size(180, 45);
+            this.siticoneButton5.TabIndex = 7;
+            this.siticoneButton5.Text = "VS";
+            this.siticoneButton5.Click += new System.EventHandler(this.siticoneButton5_Click);
+            // 
+            // siticoneButton6
+            // 
+            this.siticoneButton6.CheckedState.Parent = this.siticoneButton6;
+            this.siticoneButton6.CustomImages.Parent = this.siticoneButton6;
+            this.siticoneButton6.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.siticoneButton6.ForeColor = System.Drawing.Color.White;
+            this.siticoneButton6.HoveredState.Parent = this.siticoneButton6;
+            this.siticoneButton6.Location = new System.Drawing.Point(12, 42);
+            this.siticoneButton6.Name = "siticoneButton6";
+            this.siticoneButton6.ShadowDecoration.Parent = this.siticoneButton6;
+            this.siticoneButton6.Size = new System.Drawing.Size(180, 45);
+            this.siticoneButton6.TabIndex = 8;
+            this.siticoneButton6.Text = "Minecraft";
+            this.siticoneButton6.Click += new System.EventHandler(this.siticoneButton6_Click);
+            // 
+            // siticoneButton7
+            // 
+            this.siticoneButton7.CheckedState.Parent = this.siticoneButton7;
+            this.siticoneButton7.CustomImages.Parent = this.siticoneButton7;
+            this.siticoneButton7.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.siticoneButton7.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.siticoneButton7.ForeColor = System.Drawing.Color.White;
+            this.siticoneButton7.HoveredState.Parent = this.siticoneButton7;
+            this.siticoneButton7.Location = new System.Drawing.Point(758, 12);
+            this.siticoneButton7.Name = "siticoneButton7";
+            this.siticoneButton7.ShadowDecoration.Parent = this.siticoneButton7;
+            this.siticoneButton7.Size = new System.Drawing.Size(30, 32);
+            this.siticoneButton7.TabIndex = 9;
+            this.siticoneButton7.Text = "X";
+            this.siticoneButton7.Click += new System.EventHandler(this.siticoneButton7_Click);
+            // 
+            // siticoneButton8
+            // 
+            this.siticoneButton8.CheckedState.Parent = this.siticoneButton8;
+            this.siticoneButton8.CustomImages.Parent = this.siticoneButton8;
+            this.siticoneButton8.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.siticoneButton8.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.siticoneButton8.ForeColor = System.Drawing.Color.White;
+            this.siticoneButton8.HoveredState.Parent = this.siticoneButton8;
+            this.siticoneButton8.Location = new System.Drawing.Point(722, 12);
+            this.siticoneButton8.Name = "siticoneButton8";
+            this.siticoneButton8.ShadowDecoration.Parent = this.siticoneButton8;
+            this.siticoneButton8.Size = new System.Drawing.Size(30, 32);
+            this.siticoneButton8.TabIndex = 10;
+            this.siticoneButton8.Text = "~";
+            this.siticoneButton8.Click += new System.EventHandler(this.siticoneButton8_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(364, 195);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(291, 20);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Check my website for constant updates!";
+            // 
+            // siticoneButton9
+            // 
+            this.siticoneButton9.CheckedState.Parent = this.siticoneButton9;
+            this.siticoneButton9.CustomImages.Parent = this.siticoneButton9;
+            this.siticoneButton9.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.siticoneButton9.ForeColor = System.Drawing.Color.White;
+            this.siticoneButton9.HoveredState.Parent = this.siticoneButton9;
+            this.siticoneButton9.Location = new System.Drawing.Point(405, 235);
+            this.siticoneButton9.Name = "siticoneButton9";
+            this.siticoneButton9.ShadowDecoration.Parent = this.siticoneButton9;
+            this.siticoneButton9.Size = new System.Drawing.Size(180, 45);
+            this.siticoneButton9.TabIndex = 12;
+            this.siticoneButton9.Text = "Discord";
+            this.siticoneButton9.Click += new System.EventHandler(this.siticoneButton9_Click);
+            // 
+            // siticoneDragControl1
+            // 
+            this.siticoneDragControl1.TargetControl = this;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.siticoneButton9);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.siticoneButton8);
+            this.Controls.Add(this.siticoneButton7);
+            this.Controls.Add(this.siticoneButton6);
+            this.Controls.Add(this.siticoneButton5);
+            this.Controls.Add(this.siticoneButton4);
+            this.Controls.Add(this.siticoneButton3);
+            this.Controls.Add(this.siticoneButton2);
+            this.Controls.Add(this.siticoneButton1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form2";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private Siticone.UI.WinForms.SiticoneButton siticoneButton1;
+        private Siticone.UI.WinForms.SiticoneButton siticoneButton2;
+        private Siticone.UI.WinForms.SiticoneButton siticoneButton3;
+        private Siticone.UI.WinForms.SiticoneButton siticoneButton4;
+        private Siticone.UI.WinForms.SiticoneButton siticoneButton5;
+        private Siticone.UI.WinForms.SiticoneButton siticoneButton6;
+        private Siticone.UI.WinForms.SiticoneButton siticoneButton7;
+        private Siticone.UI.WinForms.SiticoneButton siticoneButton8;
+        private System.Windows.Forms.Label label2;
+        private Siticone.UI.WinForms.SiticoneButton siticoneButton9;
+        private Siticone.UI.WinForms.SiticoneDragControl siticoneDragControl1;
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NBT_Maker
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void siticoneButton1_Click(object sender, EventArgs e)
+        {
+            const string localFile = @"C:\Program Files\Badlion Client\Badlion Client.exe";
+            System.Diagnostics.Process.Start(localFile);
+        }
+
+        private void siticoneButton7_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void siticoneButton8_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+
+        private void siticoneButton5_Click(object sender, EventArgs e)
+        {
+            const string localFile1 = @"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\devenv.exe";
+            System.Diagnostics.Process.Start(localFile1);
+        }
+
+        private void siticoneButton4_Click(object sender, EventArgs e)
+        {
+            const string localFile2 = @"C:\Users\kyegi\AppData\Local\Programs\Microsoft VS Code\code.exe";
+            System.Diagnostics.Process.Start(localFile2);
+        }
+
+        private void siticoneButton3_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(Application.StartupPath + "\\iplookup.exe");
+        }
+
+        private void siticoneButton2_Click(object sender, EventArgs e)
+        {
+            const string localFile3 = @"C:\Users\kyegi\AppData\Local\Discord\app-1.0.9003\Discord.exe";
+            System.Diagnostics.Process.Start(localFile3);
+        }
+
+        private void siticoneButton6_Click(object sender, EventArgs e)
+        {
+            const string localFile4 = @"C:\Program Files (x86)\Minecraft Launcher\MinecraftLauncher.exe";
+            System.Diagnostics.Process.Start(localFile4);
+        }
+
+        private void siticoneButton9_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("explorer.exe", "https://www.teamg82.tk");
+        }
+    }
+}
